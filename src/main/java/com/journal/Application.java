8a37859ca0cfa7dc012622ac8a3476d2a1ce8473
GaoJean <18349349070@163.com>
2017-12-07 22:30:46 +0800

    bean 注入

--- src/main/java/com/journal/Application.java
+++ src/main/java/com/journal/Application.java
@@ -1,10 +1,9 @@
-package com.journal;
+package com.journal.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * 启动springboot
